c0657f60506a7e7bc731be15ebe806af31a38ef4
Hein Meling <hein.meling@uis.no>
2016-08-11 09:08:15 +0200

    dev/node: moved fixed code out of tmpl to new file node_func.go
    this is to avoid long tmpl files that are otherwise go code.

---
+++ dev/node_func.go
@@ -0,0 +1,137 @@
+package dev
+
+import (
+	"fmt"
+	"sort"
+	"time"
+)
+
+// ID returns the ID of m.
+func (n *Node) ID() uint32 {
+	return n.id
+}
+
+// Address returns network address of m.
+func (n *Node) Address() string {
+	return n.addr
+}
+
+func (n *Node) String() string {
+	n.Lock()
+	defer n.Unlock()
+	return fmt.Sprintf(
+		"node %d | addr: %s | latency: %v",
+		n.id, n.addr, n.latency,
+	)
+}
+
+func (n *Node) setLastErr(err error) {
+	n.Lock()
+	defer n.Unlock()
+	n.lastErr = err
+}
+
+// LastErr returns the last error encountered (if any) when invoking a remote
+// procedure call on this node.
+func (n *Node) LastErr() error {
+	n.Lock()
+	defer n.Unlock()
+	return n.lastErr
+}
+
+func (n *Node) setLatency(lat time.Duration) {
+	n.Lock()
+	defer n.Unlock()
+	n.latency = lat
+}
+
+// Latency returns the latency of the last successful remote procedure call
+// made to this node.
+func (n *Node) Latency() time.Duration {
+	n.Lock()
+	defer n.Unlock()
+	return n.latency
+}
+
+type lessFunc func(n1, n2 *Node) bool
+
+// MultiSorter implements the Sort interface, sorting the nodes within.
+type MultiSorter struct {
+	nodes []*Node
+	less  []lessFunc
+}
+
+// Sort sorts the argument slice according to the less functions passed to
+// OrderedBy.
+func (ms *MultiSorter) Sort(nodes []*Node) {
+	ms.nodes = nodes
+	sort.Sort(ms)
+}
+
+// OrderedBy returns a Sorter that sorts using the less functions, in order.
+// Call its Sort method to sort the data.
+func OrderedBy(less ...lessFunc) *MultiSorter {
+	return &MultiSorter{
+		less: less,
+	}
+}
+
+// Len is part of sort.Interface.
+func (ms *MultiSorter) Len() int {
+	return len(ms.nodes)
+}
+
+// Swap is part of sort.Interface.
+func (ms *MultiSorter) Swap(i, j int) {
+	ms.nodes[i], ms.nodes[j] = ms.nodes[j], ms.nodes[i]
+}
+
+// Less is part of sort.Interface. It is implemented by looping along the
+// less functions until it finds a comparison that is either Less or
+// !Less. Note that it can call the less functions twice per call. We
+// could change the functions to return -1, 0, 1 and reduce the
+// number of calls for greater efficiency: an exercise for the reader.
+func (ms *MultiSorter) Less(i, j int) bool {
+	p, q := ms.nodes[i], ms.nodes[j]
+	// Try all but the last comparison.
+	var k int
+	for k = 0; k < len(ms.less)-1; k++ {
+		less := ms.less[k]
+		switch {
+		case less(p, q):
+			// p < q, so we have a decision.
+			return true
+		case less(q, p):
+			// p > q, so we have a decision.
+			return false
+		}
+		// p == q; try the next comparison.
+	}
+	// All comparisons to here said "equal", so just return whatever
+	// the final comparison reports.
+	return ms.less[k](p, q)
+}
+
+// ID sorts nodes by their identifier in increasing order.
+var ID = func(n1, n2 *Node) bool {
+	return n1.id < n2.id
+}
+
+// Latency sorts nodes by latency in increasing order. Latencies less then
+// zero (sentinel value) are considered greater than any positive latency.
+var Latency = func(n1, n2 *Node) bool {
+	if n1.latency < 0 {
+		return false
+	}
+	return n1.latency < n2.latency
+
+}
+
+// Error sorts nodes by their LastErr() status in increasing order. A
+// node with LastErr() != nil is larger than a node with LastErr() == nil.
+var Error = func(n1, n2 *Node) bool {
+	if n1.lastErr != nil && n2.lastErr == nil {
+		return false
+	}
+	return true
+}
