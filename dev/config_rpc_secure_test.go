0472f953818060c6893afae7a5e44a009c9b44d6
Hein Meling <hein.meling@uis.no>
2016-08-18 23:20:45 +0100

    detour: testing authenticated channels.
    made a simple test case (reusing the same key/cert) for all servers
    just to see how much impact there is with encrypted channels.
    
    (ideally, this code should be on a separate branch, but leave it
    to @tormoder to clean that up)

---
+++ dev/config_rpc_secure_test.go
@@ -0,0 +1,171 @@
+package dev_test
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	rpc "github.com/relab/gorums/dev"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+//todo(meling) implement functionality to keep certs and keys in appropriate places.
+//todo(meling) figure out how to generate certs and keys (currently these are only google's certs and keys)
+
+const (
+	tlsDir         = "../testdata/"
+	clientCertFile = tlsDir + "ca.pem"
+	serverCertFile = tlsDir + "server1.pem"
+	serverKeyFile  = tlsDir + "server1.key"
+)
+
+//todo(meling) clean up this stuff; merge with other test setup function or something smart; perhaps splitting it up into separate functions will help with reuse.
+
+func secsetup(t testing.TB, regServers []regServer, remote bool) (regServers, rpc.ManagerOption, func(n int), func(n int)) {
+	if len(regServers) == 0 {
+		t.Fatal("setupServers: need at least one server")
+	}
+
+	creds, err := credentials.NewClientTLSFromFile(tlsDir+"ca.pem", "x.test.youtube.com")
+	if err != nil {
+		t.Fatalf("failed to create credentials %v", err)
+	}
+
+	grpcOpts := []grpc.DialOption{
+		grpc.WithBlock(),
+		grpc.WithTimeout(50 * time.Millisecond),
+		grpc.WithTransportCredentials(creds),
+	}
+	dialOpts := rpc.WithGrpcDialOptions(grpcOpts...)
+
+	if remote {
+		return regServers, dialOpts, func(int) {}, func(int) {}
+	}
+
+	servers := make([]*grpc.Server, len(regServers))
+	for i := range servers {
+		//TODO should load credentials from manager or something? or store them in Node?
+		creds, err := credentials.NewServerTLSFromFile(serverCertFile, serverKeyFile)
+		if err != nil {
+			t.Fatalf("failed to generate credentials %v", err)
+		}
+		opts := []grpc.ServerOption{grpc.Creds(creds)}
+		servers[i] = grpc.NewServer(opts...)
+	}
+	for i, server := range servers {
+		rpc.RegisterRegisterServer(server, regServers[i].implementation)
+	}
+
+	listeners := make([]net.Listener, len(servers))
+
+	for i, rs := range regServers {
+		listeners[i], err = net.Listen("tcp", rs.addr)
+		if err != nil {
+			t.Fatalf("failed to listen: %v", err)
+		}
+	}
+	for i, server := range servers {
+		go func(i int, server *grpc.Server) {
+			_ = server.Serve(listeners[i])
+		}(i, server)
+	}
+
+	for i, listener := range listeners {
+		_, port, err := net.SplitHostPort(listener.Addr().String())
+		if err != nil {
+			t.Fatalf("failed to parse listener address: %v", err)
+		}
+		regServers[i].addr = "localhost:" + port
+	}
+
+	stopGrpcServeFunc := func(n int) {
+		if n < 0 || n > len(servers) {
+			for _, s := range servers {
+				s.Stop()
+			}
+		} else {
+			servers[n].Stop()
+		}
+	}
+
+	closeListenersFunc := func(n int) {
+		if n < 0 || n > len(listeners) {
+			for _, l := range listeners {
+				l.Close()
+			}
+		} else {
+			listeners[n].Close()
+		}
+	}
+
+	return regServers, dialOpts, stopGrpcServeFunc, closeListenersFunc
+}
+
+func TestSecureRegister(t *testing.T) {
+	defer leakCheck(t)()
+
+	servers, dialOpts, stopGrpcServe, closeListeners := secsetup(
+		t,
+		[]regServer{
+			{":8080", rpc.NewRegisterBasic()},
+			{":8081", rpc.NewRegisterBasic()},
+			{":8082", rpc.NewRegisterBasic()},
+		},
+		false,
+	)
+	defer stopGrpcServe(allServers)
+
+	mgr, err := rpc.NewManager(
+		servers.addrs(),
+		dialOpts,
+	)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	defer mgr.Close()
+	closeListeners(allServers)
+
+	// Get all all available node ids
+	ids := mgr.NodeIDs()
+
+	// Quorum spec: rq=2. wq=3, n=3, sort by timestamp.
+	qspec := NewRegisterByTimestampQSpec(2, len(ids))
+
+	config, err := mgr.NewConfiguration(ids, qspec, time.Second)
+	if err != nil {
+		t.Fatalf("error creating config: %v", err)
+	}
+
+	// Test state
+	state := &rpc.State{
+		Value:     "42",
+		Timestamp: time.Now().UnixNano(),
+	}
+
+	// Perfomr write call
+	wreply, err := config.Write(state)
+	if err != nil {
+		t.Fatalf("write rpc call error: %v", err)
+	}
+	t.Logf("wreply: %v\n", wreply)
+	if !wreply.Reply.New {
+		t.Error("write reply was not marked as new")
+	}
+
+	// Do read call
+	rreply, err := config.Read(&rpc.ReadRequest{})
+	if err != nil {
+		t.Fatalf("read rpc call error: %v", err)
+	}
+	t.Logf("rreply: %v\n", rreply)
+	if rreply.Reply.Value != state.Value {
+		t.Errorf("read reply: want state %v, got %v", state, rreply.Reply)
+	}
+
+	nodes := mgr.Nodes(false)
+	for _, m := range nodes {
+		t.Logf("%v", m)
+	}
+}
