01a637052a5521254375a77cb9c9055ed6177ef0
Hein Meling <hein.meling@uis.no>
2016-08-18 08:38:59 +0100

    after rerunning GORUMSGEN=1 make gengolden

--- dev/qspec_gen.go
+++ dev/qspec_gen.go
@@ -5,6 +5,7 @@ package dev
 
 // QuorumSpec is the interface that wraps every quorum function.
 type QuorumSpec interface {
+
 	// ReadQF is the quorum function for the Read RPC method.
 	ReadQF(replies []*State) (*State, bool)
 	// WriteQF is the quorum function for the Write RPC method.
