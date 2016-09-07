7ab5317b3a0ac9d60e7790d4ce41f1da9d86323d
Tormod Erevik Lea <tormodlea@gmail.com>
2016-09-07 18:24:24 +0200

    gbench, cmd/benchclient: new package + client

---
+++ cmd/benchclient/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/relab/gorums/gbench"
+	"github.com/tylertreat/bench"
+)
+
+func main() {
+	var (
+		saddrs  = flag.String("addrs", ":8080,:8081,:8082", "server addresses seperated by ','")
+		readq   = flag.Int("rq", 2, "read quorum size")
+		psize   = flag.Int("p", 1024, "payload size in bytes")
+		timeout = flag.Duration("t", time.Second, "QRPC timeout")
+
+		brrate = flag.Uint("brrate", 10000, "benchmark request rate")
+		bconns = flag.Uint("bconns", 1, "benchmark connections (separate gorums manager&config instances)")
+		bdur   = flag.Duration("bdur", 30*time.Second, "benchmark duration")
+	)
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "\nOptions:\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	addrs := strings.Split(*saddrs, ",")
+	if len(addrs) == 0 {
+		dief("no server addresses provided")
+	}
+	if *readq > len(addrs) || *readq < 0 {
+		dief("invalid read quorum value (rq=%d, n=%d)", *readq, len(addrs))
+	}
+
+	log.SetFlags(0)
+	log.SetPrefix("benchclient: ")
+
+	r := &gbench.RequesterFactory{
+		Addrs:       addrs,
+		ReadQuorum:  *readq,
+		PayloadSize: *psize,
+		QRPCTimeout: *timeout,
+	}
+
+	benchmark := bench.NewBenchmark(r, uint64(*brrate), uint64(*bconns), *bdur)
+	start := time.Now()
+	log.Print("starting benchmark run...")
+	summary, err := benchmark.Run()
+	if err != nil {
+		log.Fatalln("benchmark error:", err)
+	}
+	log.Print("done")
+
+	log.Printf(
+		"start time: %v | #servers: %d | read quorum: %d | payload size: %d bytes",
+		start, len(addrs), *readq, *psize,
+	)
+	log.Println("summary:", summary)
+
+	filename := fmt.Sprintf(
+		"gorums-%04d%02d%02d-%02d%02d%02d.txt",
+		start.Year(), start.Month(), start.Day(),
+		start.Hour(), start.Minute(), start.Second(),
+	)
+	summary.GenerateLatencyDistribution(bench.Logarithmic, filename)
+}
+
+func dief(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
+	fmt.Fprint(os.Stderr, "\n")
+	flag.Usage()
+	os.Exit(2)
+}
---
+++ gbench/requester.go
@@ -0,0 +1,123 @@
+package gbench
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+	"time"
+
+	rpc "github.com/relab/gorums/dev"
+	"github.com/tylertreat/bench"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+)
+
+func init() {
+	silentLogger := log.New(ioutil.Discard, "", log.LstdFlags)
+	grpclog.SetLogger(silentLogger)
+	grpc.EnableTracing = false
+}
+
+type RequesterFactory struct {
+	Addrs       []string
+	ReadQuorum  int
+	PayloadSize int
+	QRPCTimeout time.Duration
+}
+
+func (r *RequesterFactory) GetRequester(uint64) bench.Requester {
+	return &gorumsRequester{
+		addrs: r.Addrs,
+		dialOpts: []grpc.DialOption{
+			grpc.WithInsecure(),
+			grpc.WithBlock(),
+			grpc.WithTimeout(time.Second),
+		},
+		readq:       r.ReadQuorum,
+		payloadSize: r.PayloadSize,
+		timeout:     r.QRPCTimeout,
+	}
+}
+
+type gorumsRequester struct {
+	addrs       []string
+	dialOpts    []grpc.DialOption
+	readq       int
+	payloadSize int
+	timeout     time.Duration
+
+	mgr    *rpc.Manager
+	config *rpc.Configuration
+}
+
+func (gr *gorumsRequester) Setup() error {
+	var err error
+	gr.mgr, err = rpc.NewManager(
+		gr.addrs,
+		rpc.WithGrpcDialOptions(gr.dialOpts...),
+	)
+	if err != nil {
+		return err
+	}
+
+	ids := gr.mgr.NodeIDs()
+	qspec := newRegisterQSpec(gr.readq, len(gr.addrs))
+	gr.config, err = gr.mgr.NewConfiguration(ids, qspec, gr.timeout)
+	if err != nil {
+		return err
+	}
+
+	// Set initial state.
+	state := &rpc.State{
+		Value:     strings.Repeat("x", gr.payloadSize),
+		Timestamp: time.Now().UnixNano(),
+	}
+	wreply, err := gr.config.Write(state)
+	if err != nil {
+		return fmt.Errorf("write rpc error: %v", err)
+	}
+	if !wreply.Reply.New {
+		return fmt.Errorf("intital write reply was not marked as new")
+	}
+
+	return nil
+}
+
+func (gr *gorumsRequester) Request() error {
+	_, err := gr.config.Read(&rpc.ReadRequest{})
+	return err
+}
+
+func (gr *gorumsRequester) Teardown() error {
+	gr.mgr.Close()
+	gr.mgr = nil
+	gr.config = nil
+	return nil
+}
+
+type registerQSpec struct {
+	rq, wq int
+}
+
+func newRegisterQSpec(rq, wq int) rpc.QuorumSpec {
+	return &registerQSpec{
+		rq: rq,
+		wq: wq,
+	}
+}
+
+func (rqs *registerQSpec) ReadQF(replies []*rpc.State) (*rpc.State, bool) {
+	if len(replies) < rqs.rq {
+		return nil, false
+	}
+	return replies[0], true
+}
+
+func (rqs *registerQSpec) WriteQF(replies []*rpc.WriteResponse) (*rpc.WriteResponse, bool) {
+	if len(replies) < rqs.wq {
+		return nil, false
+	}
+	return replies[0], true
+}
