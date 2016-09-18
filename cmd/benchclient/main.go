efe99dc93a3800bf1a45a5e79d4061fd48301e2f
Hein Meling <hein.meling@uis.no>
2016-09-18 10:08:21 +0200

    fixed bug in determining f when -addrs were specified

--- cmd/benchclient/main.go
+++ cmd/benchclient/main.go
@@ -137,8 +137,8 @@ func main() {
 		benchParams = fmt.Sprintf("%s | readq: %d", benchParams, *readq)
 		params = fmt.Sprintf("%s-RQ%d", params, *readq)
 	case byzq:
-		benchParams = fmt.Sprintf("%s | f: %d", benchParams, *f)
-		params = fmt.Sprintf("%s-F%d", params, *f)
+		benchParams = fmt.Sprintf("%s | f: %d", benchParams, len(addrs)-1/3)
+		params = fmt.Sprintf("%s-F%d", params, len(addrs)-1/3)
 	}
 	log.Print(benchParams)
 	log.Println("summary:", summary)
