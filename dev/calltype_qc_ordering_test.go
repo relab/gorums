e23441d6eea709ff501f7519b9f32eee959d2d34
Tormod Erevik Lea <tormodlea@gmail.com>
2017-09-07 12:30:25 +0200

    dev: add calltype_qc_ordering_test.go
    Updates #16.

---
+++ dev/calltype_qc_ordering_test.go
@@ -0,0 +1,163 @@
+// +build order
+
+package dev_test
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+
+	qc "github.com/relab/gorums/dev"
+	"golang.org/x/net/context"
+)
+
+func TestQuorumCallOrdering(t *testing.T) {
+	defer leakCheck(t)()
+	servers, dialOpts, stopGrpcServe, closeListeners := setup(
+		t,
+		[]regServer{
+			{impl: newStorageServerRequiringOrdering(t)},
+			{impl: newStorageServerRequiringOrdering(t)},
+			{impl: newStorageServerRequiringOrdering(t)},
+			{impl: newStorageServerRequiringOrdering(t)},
+			{impl: newStorageServerRequiringOrdering(t)},
+		},
+		false,
+	)
+	defer closeListeners(allServers)
+	defer stopGrpcServe(allServers)
+
+	mgrOpts := []qc.ManagerOption{
+		dialOpts,
+	}
+	mgr, err := qc.NewManager(
+		servers.addrs(),
+		mgrOpts...,
+	)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	defer mgr.Close()
+
+	ids := mgr.NodeIDs()
+
+	// Quorum spec: rq=1 (not used), wq=3, n=5.
+	qspec := NewStorageQSpec(1, 2)
+
+	config, err := mgr.NewConfiguration(ids, qspec)
+	if err != nil {
+		t.Fatalf("error creating config: %v", err)
+	}
+
+	state := &qc.State{
+		Value:     "42",
+		Timestamp: 1, // Servers start at 0.
+	}
+
+	const maxNumberOfQCs = 1 << 18
+
+	// Test description:
+	//
+	// Sequentially call Write with an increasing timestamp.
+	//
+	// n=5, wq=3 -> Write QC will return after three replies.
+	//
+	// Do _not_ cancel context to allow the two remaining goroutines per
+	// call to be handled on the servers.
+	//
+	// Servers will report error if a message received out-of-order.
+	for i := 1; i < maxNumberOfQCs; i++ {
+		_, err := config.Write(context.Background(), state)
+		if err != nil {
+			t.Fatalf("write quorum call error: %v", err)
+		}
+		state.Timestamp++
+	}
+}
+
+// StorageServerRequiringOrdering represents a single state storage that
+// requires strict ordering of received messages.
+type storageServerRequiringOrdering struct {
+	mu        sync.Mutex
+	state     qc.State
+	writeResp qc.WriteResponse
+	t         *testing.T
+}
+
+// NewStorageBasic returns a new basic storage server.
+func newStorageServerRequiringOrdering(t *testing.T) *storageServerRequiringOrdering {
+	return &storageServerRequiringOrdering{t: t}
+}
+
+// Read implements the Read method.
+func (s *storageServerRequiringOrdering) Read(ctx context.Context, rq *qc.ReadRequest) (*qc.State, error) {
+	panic("not implemented")
+}
+
+// ReadCorrectable implements the ReadCorrectable method.
+func (s *storageServerRequiringOrdering) ReadCorrectable(ctx context.Context, rq *qc.ReadRequest) (*qc.State, error) {
+	panic("not implemented")
+}
+
+// ReadFuture implements the ReadFuture method.
+func (s *storageServerRequiringOrdering) ReadFuture(ctx context.Context, rq *qc.ReadRequest) (*qc.State, error) {
+	panic("not implemented")
+}
+
+// ReadCustomReturn implements the ReadCustomReturn method.
+func (s *storageServerRequiringOrdering) ReadCustomReturn(ctx context.Context, rq *qc.ReadRequest) (*qc.State, error) {
+	panic("not implemented")
+}
+
+// Write implements the Write method.
+func (s *storageServerRequiringOrdering) Write(ctx context.Context, state *qc.State) (*qc.WriteResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if state.Timestamp != s.state.Timestamp+1 {
+		// TODO(tormoder): Why does not calling t.Errorf (or anything on t) have an effect?
+		// Panic for now.
+		err := fmt.Sprintf(
+			"server: message received out-of-order: got: %d, expected: %d",
+			state.Timestamp, s.state.Timestamp+1,
+		)
+		panic(err)
+	} else {
+		s.state = *state
+	}
+	return &s.writeResp, nil
+}
+
+// WriteFuture implements the WriteFuture method.
+func (s *storageServerRequiringOrdering) WriteFuture(ctx context.Context, state *qc.State) (*qc.WriteResponse, error) {
+	panic("not implemented")
+}
+
+// WritePerNode implements the WritePerNode method.
+func (s *storageServerRequiringOrdering) WritePerNode(ctx context.Context, state *qc.State) (*qc.WriteResponse, error) {
+	panic("not implemented")
+}
+
+// WriteAsync implements the WriteAsync method from the StorageServer interface.
+func (s *storageServerRequiringOrdering) WriteAsync(stream qc.Storage_WriteAsyncServer) error {
+	return nil
+}
+
+// ReadNoQC implements the ReadNoQC method from the StorageServer interface.
+func (s *storageServerRequiringOrdering) ReadNoQC(ctx context.Context, rq *qc.ReadRequest) (*qc.State, error) {
+	panic("not implemented")
+}
+
+// ReadPrelim implements the ReadPrelim method from the StorageServer interface.
+func (s *storageServerRequiringOrdering) ReadPrelim(rq *qc.ReadRequest, rrts qc.Storage_ReadPrelimServer) error {
+	panic("not implemented")
+}
+
+// ReadExecuted returns when r has has completed a read.
+func (s *storageServerRequiringOrdering) ReadExecuted() {
+	panic("not implemented")
+}
+
+// WriteExecuted returns when r has has completed a write.
+func (s *storageServerRequiringOrdering) WriteExecuted() {
+	panic("not implemented")
+}
