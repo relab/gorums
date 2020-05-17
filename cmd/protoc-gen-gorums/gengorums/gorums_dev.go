7030d04c0e9e5a3ac2d4f0d85562872be9e1f061
Hein Meling <hein.meling@uis.no>
2020-05-17 15:47:31 +0200

    unified gorums file guard
    This commit simplifies and unifies the guard for gorums proto
    files, meaning that we now also check for Gorums reserved idents
    in the proto message types also for dev files. Moreover, we
    simplified the checking for Gorums method types in the proto
    file; now there is only one method to do this instead of two.

--- cmd/protoc-gen-gorums/gengorums/gorums_dev.go
+++ cmd/protoc-gen-gorums/gengorums/gorums_dev.go
@@ -1,34 +1,21 @@
 package gengorums
 
 import (
-	"fmt"
-	"log"
-	"os"
-
 	"google.golang.org/protobuf/compiler/protogen"
 )
 
 // GenerateDevFiles generates a zorums_{{gorumsType}}_gorums.pb.go file for each Gorums datatype
 // and for each call type in the service definition.
 func GenerateDevFiles(gen *protogen.Plugin, file *protogen.File) {
+	if !gorumsGuard(file) {
+		return
+	}
 	for gorumsType := range gorumsCallTypesInfo {
 		generateDevFile(gen, file, gorumsType)
 	}
 }
 
 func generateDevFile(gen *protogen.Plugin, file *protogen.File, gorumsType string) {
-	if len(file.Services) == 0 || !hasGorumsType(file.Services, gorumsType) {
-		// there is nothing for this plugin to do
-		fmt.Fprintf(os.Stderr, "ignoring %s\n", gorumsType)
-		return
-	}
-	if len(file.Services) > 1 {
-		// To build multiple services, make separate proto files and
-		// run the plugin separately for each proto file.
-		// These cannot share the same Go package.
-		log.Fatalln("Gorums does not support multiple services in the same proto file.")
-	}
-
 	// generate dev file for given gorumsType
 	filename := file.GeneratedFilenamePrefix + "_" + gorumsType + "_gorums.pb.go"
 	g := gen.NewGeneratedFile(filename, file.GoImportPath)
