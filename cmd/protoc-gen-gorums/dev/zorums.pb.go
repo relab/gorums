dd432d5166eb02ee8f09afb79cfe579c83be9358
John Ingve Olsen <johningveolsen@gmail.com>
2020-05-29 14:58:19 +0200

    Add a unicast calltype
    This adds a new unicast calltype that can be used by specifying the
    gorums.unicast option. This unicast calltype uses the same underlying
    NodeStream as the ordered quorum call variants.

--- cmd/protoc-gen-gorums/dev/zorums.pb.go
+++ cmd/protoc-gen-gorums/dev/zorums.pb.go
@@ -181,7 +181,7 @@ var file_zorums_proto_rawDesc = []byte{
 	0x65, 0x73, 0x75, 0x6c, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x06, 0x52, 0x65, 0x73,
 	0x75, 0x6c, 0x74, 0x22, 0x22, 0x0a, 0x0a, 0x4d, 0x79, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73,
 	0x65, 0x12, 0x14, 0x0a, 0x05, 0x56, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
-	0x52, 0x05, 0x56, 0x61, 0x6c, 0x75, 0x65, 0x32, 0xa6, 0x15, 0x0a, 0x0d, 0x5a, 0x6f, 0x72, 0x75,
+	0x52, 0x05, 0x56, 0x61, 0x6c, 0x75, 0x65, 0x32, 0x8c, 0x16, 0x0a, 0x0d, 0x5a, 0x6f, 0x72, 0x75,
 	0x6d, 0x73, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x29, 0x0a, 0x08, 0x47, 0x52, 0x50,
 	0x43, 0x43, 0x61, 0x6c, 0x6c, 0x12, 0x0c, 0x2e, 0x64, 0x65, 0x76, 0x2e, 0x52, 0x65, 0x71, 0x75,
 	0x65, 0x73, 0x74, 0x1a, 0x0d, 0x2e, 0x64, 0x65, 0x76, 0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e,
@@ -352,9 +352,15 @@ var file_zorums_proto_rawDesc = []byte{
 	0x74, 0x1a, 0x0d, 0x2e, 0x64, 0x65, 0x76, 0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
 	0x22, 0x1e, 0x80, 0xb5, 0x18, 0x01, 0x88, 0xb5, 0x18, 0x01, 0x90, 0xb5, 0x18, 0x01, 0xd0, 0xb5,
 	0x18, 0x01, 0xc2, 0xf3, 0x18, 0x0a, 0x4d, 0x79, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
-	0x42, 0x1b, 0x5a, 0x19, 0x63, 0x6d, 0x64, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x2d, 0x67,
-	0x65, 0x6e, 0x2d, 0x67, 0x6f, 0x72, 0x75, 0x6d, 0x73, 0x2f, 0x64, 0x65, 0x76, 0x62, 0x06, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x12, 0x2c, 0x0a, 0x07, 0x55, 0x6e, 0x69, 0x63, 0x61, 0x73, 0x74, 0x12, 0x0c, 0x2e, 0x64, 0x65,
+	0x76, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x0d, 0x2e, 0x64, 0x65, 0x76, 0x2e,
+	0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x04, 0xa8, 0xb5, 0x18, 0x01, 0x12, 0x36,
+	0x0a, 0x08, 0x55, 0x6e, 0x69, 0x63, 0x61, 0x73, 0x74, 0x32, 0x12, 0x0c, 0x2e, 0x64, 0x65, 0x76,
+	0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x16, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c,
+	0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79,
+	0x22, 0x04, 0xa8, 0xb5, 0x18, 0x01, 0x42, 0x1b, 0x5a, 0x19, 0x63, 0x6d, 0x64, 0x2f, 0x70, 0x72,
+	0x6f, 0x74, 0x6f, 0x63, 0x2d, 0x67, 0x65, 0x6e, 0x2d, 0x67, 0x6f, 0x72, 0x75, 0x6d, 0x73, 0x2f,
+	0x64, 0x65, 0x76, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -417,48 +423,52 @@ var file_zorums_proto_depIdxs = []int32{
 	0,  // 37: dev.ZorumsService.OrderingFuturePerNodeArg:input_type -> dev.Request
 	0,  // 38: dev.ZorumsService.OrderingFutureCustomReturnType:input_type -> dev.Request
 	0,  // 39: dev.ZorumsService.OrderingFutureCombo:input_type -> dev.Request
-	1,  // 40: dev.ZorumsService.GRPCCall:output_type -> dev.Response
-	1,  // 41: dev.ZorumsService.QuorumCall:output_type -> dev.Response
-	1,  // 42: dev.ZorumsService.QuorumCallPerNodeArg:output_type -> dev.Response
-	1,  // 43: dev.ZorumsService.QuorumCallCustomReturnType:output_type -> dev.Response
-	1,  // 44: dev.ZorumsService.QuorumCallCombo:output_type -> dev.Response
-	1,  // 45: dev.ZorumsService.QuorumCallEmpty:output_type -> dev.Response
-	3,  // 46: dev.ZorumsService.QuorumCallEmpty2:output_type -> google.protobuf.Empty
-	1,  // 47: dev.ZorumsService.Multicast:output_type -> dev.Response
-	1,  // 48: dev.ZorumsService.MulticastPerNodeArg:output_type -> dev.Response
-	1,  // 49: dev.ZorumsService.Multicast2:output_type -> dev.Response
-	3,  // 50: dev.ZorumsService.Multicast3:output_type -> google.protobuf.Empty
-	3,  // 51: dev.ZorumsService.Multicast4:output_type -> google.protobuf.Empty
-	1,  // 52: dev.ZorumsService.QuorumCallFuture:output_type -> dev.Response
-	1,  // 53: dev.ZorumsService.QuorumCallFuturePerNodeArg:output_type -> dev.Response
-	1,  // 54: dev.ZorumsService.QuorumCallFutureCustomReturnType:output_type -> dev.Response
-	1,  // 55: dev.ZorumsService.QuorumCallFutureCombo:output_type -> dev.Response
-	1,  // 56: dev.ZorumsService.QuorumCallFuture2:output_type -> dev.Response
-	3,  // 57: dev.ZorumsService.QuorumCallFutureEmpty:output_type -> google.protobuf.Empty
-	1,  // 58: dev.ZorumsService.QuorumCallFutureEmpty2:output_type -> dev.Response
-	1,  // 59: dev.ZorumsService.Correctable:output_type -> dev.Response
-	1,  // 60: dev.ZorumsService.CorrectablePerNodeArg:output_type -> dev.Response
-	1,  // 61: dev.ZorumsService.CorrectableCustomReturnType:output_type -> dev.Response
-	1,  // 62: dev.ZorumsService.CorrectableCombo:output_type -> dev.Response
-	3,  // 63: dev.ZorumsService.CorrectableEmpty:output_type -> google.protobuf.Empty
-	1,  // 64: dev.ZorumsService.CorrectableEmpty2:output_type -> dev.Response
-	1,  // 65: dev.ZorumsService.CorrectableStream:output_type -> dev.Response
-	1,  // 66: dev.ZorumsService.CorrectableStreamPerNodeArg:output_type -> dev.Response
-	1,  // 67: dev.ZorumsService.CorrectableStreamCustomReturnType:output_type -> dev.Response
-	1,  // 68: dev.ZorumsService.CorrectableStreamCombo:output_type -> dev.Response
-	3,  // 69: dev.ZorumsService.CorrectableStreamEmpty:output_type -> google.protobuf.Empty
-	1,  // 70: dev.ZorumsService.CorrectableStreamEmpty2:output_type -> dev.Response
-	1,  // 71: dev.ZorumsService.OrderingQC:output_type -> dev.Response
-	1,  // 72: dev.ZorumsService.OrderingPerNodeArg:output_type -> dev.Response
-	1,  // 73: dev.ZorumsService.OrderingCustomReturnType:output_type -> dev.Response
-	1,  // 74: dev.ZorumsService.OrderingCombo:output_type -> dev.Response
-	1,  // 75: dev.ZorumsService.OrderingUnaryRPC:output_type -> dev.Response
-	1,  // 76: dev.ZorumsService.OrderingFuture:output_type -> dev.Response
-	1,  // 77: dev.ZorumsService.OrderingFuturePerNodeArg:output_type -> dev.Response
-	1,  // 78: dev.ZorumsService.OrderingFutureCustomReturnType:output_type -> dev.Response
-	1,  // 79: dev.ZorumsService.OrderingFutureCombo:output_type -> dev.Response
-	40, // [40:80] is the sub-list for method output_type
-	0,  // [0:40] is the sub-list for method input_type
+	0,  // 40: dev.ZorumsService.Unicast:input_type -> dev.Request
+	0,  // 41: dev.ZorumsService.Unicast2:input_type -> dev.Request
+	1,  // 42: dev.ZorumsService.GRPCCall:output_type -> dev.Response
+	1,  // 43: dev.ZorumsService.QuorumCall:output_type -> dev.Response
+	1,  // 44: dev.ZorumsService.QuorumCallPerNodeArg:output_type -> dev.Response
+	1,  // 45: dev.ZorumsService.QuorumCallCustomReturnType:output_type -> dev.Response
+	1,  // 46: dev.ZorumsService.QuorumCallCombo:output_type -> dev.Response
+	1,  // 47: dev.ZorumsService.QuorumCallEmpty:output_type -> dev.Response
+	3,  // 48: dev.ZorumsService.QuorumCallEmpty2:output_type -> google.protobuf.Empty
+	1,  // 49: dev.ZorumsService.Multicast:output_type -> dev.Response
+	1,  // 50: dev.ZorumsService.MulticastPerNodeArg:output_type -> dev.Response
+	1,  // 51: dev.ZorumsService.Multicast2:output_type -> dev.Response
+	3,  // 52: dev.ZorumsService.Multicast3:output_type -> google.protobuf.Empty
+	3,  // 53: dev.ZorumsService.Multicast4:output_type -> google.protobuf.Empty
+	1,  // 54: dev.ZorumsService.QuorumCallFuture:output_type -> dev.Response
+	1,  // 55: dev.ZorumsService.QuorumCallFuturePerNodeArg:output_type -> dev.Response
+	1,  // 56: dev.ZorumsService.QuorumCallFutureCustomReturnType:output_type -> dev.Response
+	1,  // 57: dev.ZorumsService.QuorumCallFutureCombo:output_type -> dev.Response
+	1,  // 58: dev.ZorumsService.QuorumCallFuture2:output_type -> dev.Response
+	3,  // 59: dev.ZorumsService.QuorumCallFutureEmpty:output_type -> google.protobuf.Empty
+	1,  // 60: dev.ZorumsService.QuorumCallFutureEmpty2:output_type -> dev.Response
+	1,  // 61: dev.ZorumsService.Correctable:output_type -> dev.Response
+	1,  // 62: dev.ZorumsService.CorrectablePerNodeArg:output_type -> dev.Response
+	1,  // 63: dev.ZorumsService.CorrectableCustomReturnType:output_type -> dev.Response
+	1,  // 64: dev.ZorumsService.CorrectableCombo:output_type -> dev.Response
+	3,  // 65: dev.ZorumsService.CorrectableEmpty:output_type -> google.protobuf.Empty
+	1,  // 66: dev.ZorumsService.CorrectableEmpty2:output_type -> dev.Response
+	1,  // 67: dev.ZorumsService.CorrectableStream:output_type -> dev.Response
+	1,  // 68: dev.ZorumsService.CorrectableStreamPerNodeArg:output_type -> dev.Response
+	1,  // 69: dev.ZorumsService.CorrectableStreamCustomReturnType:output_type -> dev.Response
+	1,  // 70: dev.ZorumsService.CorrectableStreamCombo:output_type -> dev.Response
+	3,  // 71: dev.ZorumsService.CorrectableStreamEmpty:output_type -> google.protobuf.Empty
+	1,  // 72: dev.ZorumsService.CorrectableStreamEmpty2:output_type -> dev.Response
+	1,  // 73: dev.ZorumsService.OrderingQC:output_type -> dev.Response
+	1,  // 74: dev.ZorumsService.OrderingPerNodeArg:output_type -> dev.Response
+	1,  // 75: dev.ZorumsService.OrderingCustomReturnType:output_type -> dev.Response
+	1,  // 76: dev.ZorumsService.OrderingCombo:output_type -> dev.Response
+	1,  // 77: dev.ZorumsService.OrderingUnaryRPC:output_type -> dev.Response
+	1,  // 78: dev.ZorumsService.OrderingFuture:output_type -> dev.Response
+	1,  // 79: dev.ZorumsService.OrderingFuturePerNodeArg:output_type -> dev.Response
+	1,  // 80: dev.ZorumsService.OrderingFutureCustomReturnType:output_type -> dev.Response
+	1,  // 81: dev.ZorumsService.OrderingFutureCombo:output_type -> dev.Response
+	1,  // 82: dev.ZorumsService.Unicast:output_type -> dev.Response
+	3,  // 83: dev.ZorumsService.Unicast2:output_type -> google.protobuf.Empty
+	42, // [42:84] is the sub-list for method output_type
+	0,  // [0:42] is the sub-list for method input_type
 	0,  // [0:0] is the sub-list for extension type_name
 	0,  // [0:0] is the sub-list for extension extendee
 	0,  // [0:0] is the sub-list for field type_name
---
+++ cmd/protoc-gen-gorums/gengorums/template_unicast.go
@@ -0,0 +1,24 @@
+package gengorums
+
+var unicastMethod = `
+func (n *Node) {{$method}}(in *{{$in}}) error {
+	msgID := n.nextMsgID()
+	data, err := {{$marshalOptions}}{AllowPartial: true, Deterministic: true}.Marshal(in)
+	if err != nil {
+		return {{$errorf}}("failed to marshal message: %w", err)
+	}
+	msg := &{{$gorumsMsg}}{
+		ID: msgID,
+		MethodID: {{$unexportMethod}}MethodID,
+		Data: data,
+	}
+	n.sendQ <- msg
+	return nil
+}
+`
+
+var unicastCall = commonVariables +
+	orderingVariables +
+	multicastRefImports +
+	unicastMethod +
+	multicastHandler
