3ddcc53064ebcee6d9f8288abb0b14a3786dc41b
John Ingve Olsen <johningveolsen@gmail.com>
2020-06-24 10:18:43 +0200

    removed an unused struct field

--- cmd/protoc-gen-gorums/gengorums/template_static.go
+++ cmd/protoc-gen-gorums/gengorums/template_static.go
@@ -1138,7 +1138,6 @@ func WithGRPCServerOptions(opts ...grpc.ServerOption) ServerOption {
 type GorumsServer struct {
 	srv        *orderingServer
 	grpcServer *grpc.Server
-	opts       serverOptions
 }
 
 // NewGorumsServer returns a new instance of GorumsServer.
