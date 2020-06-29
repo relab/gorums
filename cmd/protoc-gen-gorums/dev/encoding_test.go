86b66b3c20d87fdefe23ad61c3cf63452df04ca7
John Ingve Olsen <johningveolsen@gmail.com>
2020-06-29 07:42:11 +0200

    test that codec can marshal normal Protobufs
    The codec should also support marshaling normal protobuf messages, which
    may be necessary if there are other gRPC services running on the same
    server.

--- cmd/protoc-gen-gorums/dev/encoding_test.go
+++ cmd/protoc-gen-gorums/dev/encoding_test.go
@@ -18,24 +18,24 @@ var (
 func TestMarshalGorumsMessage(t *testing.T) {
 	_, err := codec.Marshal(testMsg)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 }
 
 func TestUnmarshalGorumsMessage(t *testing.T) {
 	buf, err := codec.Marshal(testMsg)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	msg := &gorumsMessage{metadata: &ordering.Metadata{}, message: &Request{}}
 	err = codec.Unmarshal(buf, msg)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if msg.metadata.MessageID != 1 || msg.metadata.MethodID != 2 || msg.message.(*Request).Value != "foo bar" {
-		t.Errorf("Failed to unmarshal message correctly.")
+		t.Fatal("Failed to unmarshal message correctly.")
 	}
 }
 
@@ -43,6 +43,24 @@ func TestMarshalUnsupportedType(t *testing.T) {
 	now := time.Now()
 	_, err := codec.Marshal(now)
 	if err == nil {
-		t.Error("Expected error from gorumsEncoder since marshalling unsupported type")
+		t.Fatal("Expected error from gorumsEncoder since marshalling unsupported type")
+	}
+}
+
+// Test that marshaling normal Protobuf message types works
+func TestMarshalAndUnmarshalProtobuf(t *testing.T) {
+	buf, err := codec.Marshal(testMsg.message)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msg := &Request{}
+	err = codec.Unmarshal(buf, msg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if msg.Value != testMsg.message.(*Request).Value {
+		t.Fatal("Failed to unmarshal message correctly.")
 	}
 }
