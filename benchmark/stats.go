0175fc2eb11a336bcba01abeb8286ea05878993c
John Ingve Olsen <johningveolsen@gmail.com>
2020-06-29 15:19:08 +0200

    benchmark: Incorporate Welford's algorithm

--- benchmark/stats.go
+++ benchmark/stats.go
@@ -7,8 +7,6 @@ import (
 	"strings"
 	"sync"
 	"time"
-
-	"github.com/eclesh/welford"
 )
 
 // Format returns a tab formatted string representation of the result
@@ -26,11 +24,13 @@ func (r *Result) Format() string {
 // Stats records and processes the raw data of a benchmark
 type Stats struct {
 	mut       sync.Mutex
-	stats     welford.Stats
 	startTime time.Time
 	endTime   time.Time
 	startMs   runtime.MemStats
 	endMs     runtime.MemStats
+
+	count    uint64
+	mean, m2 float64
 }
 
 // Start records the start time and memory stats
@@ -56,7 +56,12 @@ func (s *Stats) AddLatency(l time.Duration) {
 	s.mut.Lock()
 	defer s.mut.Unlock()
 
-	s.stats.Add(float64(l))
+	// implements Welford's algorithm
+	s.count++
+	delta := float64(l) - s.mean
+	s.mean += delta / float64(s.count)
+	delta2 := float64(l) - s.mean
+	s.m2 += delta * delta2
 }
 
 // GetResult computes and returns the result of the benchmark
@@ -65,11 +70,13 @@ func (s *Stats) GetResult() *Result {
 	defer s.mut.Unlock()
 
 	r := &Result{}
-	r.TotalOps = s.stats.Count()
+	r.TotalOps = s.count
 	r.TotalTime = int64(s.endTime.Sub(s.startTime))
 	r.Throughput = float64(r.TotalOps) / float64(time.Duration(r.TotalTime).Seconds())
-	r.LatencyAvg = s.stats.Mean()
-	r.LatencyVar = s.stats.Variance()
+	r.LatencyAvg = s.mean
+	if s.count > 2 {
+		r.LatencyVar = s.m2 / float64(s.count-1)
+	}
 	r.AllocsPerOp = (s.endMs.Mallocs - s.startMs.Mallocs) / r.TotalOps
 	r.MemPerOp = (s.endMs.TotalAlloc - s.startMs.TotalAlloc) / r.TotalOps
 	return r
@@ -78,10 +85,12 @@ func (s *Stats) GetResult() *Result {
 // Clear zeroes out the stats
 func (s *Stats) Clear() {
 	s.mut.Lock()
-	s.stats.Reset()
 	s.startTime = time.Time{}
 	s.endTime = time.Time{}
 	s.startMs = runtime.MemStats{}
 	s.endMs = runtime.MemStats{}
+	s.count = 0
+	s.mean = 0
+	s.m2 = 0
 	s.mut.Unlock()
 }
