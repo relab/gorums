ae3f6ea0d07f9db4acd7f633c8566c81bcf46270
John Ingve Olsen <johningveolsen@gmail.com>
2020-07-07 16:01:47 +0200

    Make TestSetup work for generic server types
    TestSetup can now work with any server type that implements the Serve()
    and Stop() functions.

--- testing_gorums.go
+++ testing_gorums.go
@@ -5,9 +5,12 @@ import (
 	"net"
 	"sync"
 	"testing"
-
-	"google.golang.org/grpc"
 )
+
+type server interface {
+	Serve(net.Listener) error
+	Stop()
+}
 
 // Package testing provide a public API for setting up Gorums.
 // This package can be used by other packages, such as Raft and HotStuff.
@@ -15,13 +18,16 @@ import (
 // TestSetup starts numServers gRPC servers using the given registration
 // function, and returns the server addresses along with a stop function
 // that should be called to shut down the test.
-func TestSetup(t testing.TB, numServers int, regSrvFn func(*grpc.Server)) ([]string, func()) {
+func TestSetup(t testing.TB, numServers int, srvFunc func() interface{}) ([]string, func()) {
 	t.Helper()
-	servers := make([]*grpc.Server, numServers)
+	servers := make([]server, numServers)
 	addrs := make([]string, numServers)
 	for i := 0; i < numServers; i++ {
-		srv := grpc.NewServer()
-		regSrvFn(srv)
+		var srv server
+		var ok bool
+		if srv, ok = srvFunc().(server); !ok {
+			t.Fatal("Incompatible server type. You should use a GorumsServer or grpc.Server")
+		}
 		lis, err := getListener()
 		if err != nil {
 			t.Fatalf("Failed to listen on port: %v", err)
