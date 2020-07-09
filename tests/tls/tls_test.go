635d58cfbc258b71918f99b74fac6f5cc21d5b7e
John Ingve Olsen <johningveolsen@gmail.com>
2020-07-09 10:27:30 +0200

    Added a test for TLS
    The server-side metadata allows us to check the auth type of the client.
    This commit adds a test to see if TLS is working with Gorums.
    Also, turns out that the reason the old TLS test broke was because the
    included cert had expired. To avoid this problem in the future, the test
    now generates its own certificate.

---
+++ tests/tls/tls_test.go
@@ -0,0 +1,69 @@
+package tls
+
+import (
+	context "context"
+	"crypto/tls"
+	"crypto/x509"
+	"testing"
+	"time"
+
+	"github.com/relab/gorums"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/peer"
+)
+
+type testSrv struct{}
+
+func (t testSrv) TestTLS(ctx context.Context, in *Request, out func(*Response)) {
+	peerInfo, ok := peer.FromContext(ctx)
+	if !ok || peerInfo.AuthInfo.AuthType() != "tls" {
+		out(&Response{OK: false})
+		return
+	}
+	out(&Response{OK: true})
+}
+
+func TestTLS(t *testing.T) {
+	cert, key, err := generateCert()
+	if err != nil {
+		t.Errorf("Failed to generate certificate: %v", err)
+	}
+
+	cp := x509.NewCertPool()
+	if !cp.AppendCertsFromPEM(cert) {
+		t.Errorf("Failed to add cert to pool.")
+	}
+
+	tlsCert, err := tls.X509KeyPair(cert, key)
+	if err != nil {
+		t.Errorf("Failed to parse cert: %v", err)
+	}
+
+	addrs, teardown := gorums.TestSetup(t, 1, func() interface{} {
+		srv := NewGorumsServer(WithGRPCServerOptions(grpc.Creds(credentials.NewServerTLSFromCert(&tlsCert))))
+		srv.RegisterTLSServer(&testSrv{})
+		return srv
+	})
+	defer teardown()
+
+	mgr, err := NewManager(WithNodeList(addrs), WithDialTimeout(100*time.Millisecond), WithGrpcDialOptions(
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(cp, "")),
+		grpc.WithReturnConnectionError(),
+	))
+	if err != nil {
+		t.Fatalf("Failed to start manager: %v", err)
+	}
+
+	node := mgr.Nodes()[0]
+
+	resp, err := node.TestTLS(context.Background(), &Request{})
+	if err != nil {
+		t.Fatalf("Failed to perform RPC: %v", err)
+	}
+
+	if !resp.GetOK() {
+		t.Fatalf("TLS was not enabled")
+	}
+}
