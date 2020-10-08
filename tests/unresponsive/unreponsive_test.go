5dcebe533935b583a6dfc4c6f7bda9ac543f178d
John Ingve Olsen <johningveolsen@gmail.com>
2020-10-08 23:16:14 +0200

    Test that client is not blocked by unresponsive server
    When a server is not calling RecvMsg(), the calls to SendMsg() will
    eventually block. This test makes sure that we don't block forever.

---
+++ tests/unresponsive/unreponsive_test.go
@@ -0,0 +1,48 @@
+package unresponsive
+
+import (
+	context "context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/relab/gorums"
+	"google.golang.org/grpc"
+)
+
+type testSrv struct{}
+
+func (srv testSrv) TestUnresponsive(ctx context.Context, _ *Empty, _ func(*Empty, error)) {
+	<-ctx.Done()
+}
+
+// TestUnresponsive checks that the client is not blocked when the server is not receiving messages
+func TestUnresponsive(t *testing.T) {
+	addrs, teardown := gorums.TestSetup(t, 1, func() interface{} {
+		gorumsSrv := gorums.NewServer()
+		srv := &testSrv{}
+		RegisterUnresponsiveServer(gorumsSrv, srv)
+		return gorumsSrv
+	})
+	defer teardown()
+
+	mgr, err := NewManager(
+		gorums.WithNodeList(addrs),
+		gorums.WithDialTimeout(100*time.Millisecond),
+		gorums.WithGrpcDialOptions(grpc.WithInsecure(), grpc.WithBlock()),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	node := mgr.Nodes()[0]
+
+	for i := 0; i < 1000; i++ {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+		_, err = node.TestUnresponsive(ctx, &Empty{})
+		if err != nil && errors.Is(err, context.Canceled) {
+			t.Error(err)
+		}
+		cancel()
+	}
+}
