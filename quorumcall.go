aa8e0809cb685576788ff7b782a429ca53a0ba05
Hein Meling <hein.meling@uis.no>
2020-12-21 20:53:13 +0100

    Fixed minor typo

--- quorumcall.go
+++ quorumcall.go
@@ -21,7 +21,7 @@ func QuorumCall(ctx context.Context, d QuorumCallData) (resp protoreflect.ProtoM
 	// set up channel to collect replies to this call.
 	replyChan := make(chan *gorumsStreamResult, len(d.Nodes))
 	d.Manager.putChan(msgID, replyChan)
-	// and remove it when the call it scomplete
+	// and remove it when the call is complete
 	defer d.Manager.deleteChan(msgID)
 
 	md := &ordering.Metadata{
