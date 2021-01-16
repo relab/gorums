42d37df9b29cf6d407487b6eddb40acad8df53db
Hein Meling <hein.meling@uis.no>
2021-01-16 21:16:21 +0100

    Added documentation comments to Multicast

--- multicast.go
+++ multicast.go
@@ -4,8 +4,13 @@ import (
 	"context"
 )
 
+// Multicast is a one-way call; no replies are processed.
+// Providing the call option WithAsyncSend, the function may return
+// before the message has been sent. Without this call option, the
+// function waits until the message has been sent to all nodes.
 func Multicast(ctx context.Context, d QuorumCallData, opts ...CallOption) {
 	o := getCallOptions(E_Multicast, opts)
+	// sendAsync == true => replyChan and callDone are nil and thus cannot be used
 	md, replyChan, callDone := d.Manager.newCall(d.Method, len(d.Nodes), !o.sendAsync)
 
 	for _, n := range d.Nodes {
@@ -19,8 +24,9 @@ func Multicast(ctx context.Context, d QuorumCallData, opts ...CallOption) {
 		n.sendQ <- gorumsStreamRequest{ctx: ctx, msg: &Message{Metadata: md, Message: msg}, opts: o}
 	}
 
-	// wait until the messages have been sent (nodeStream will give empty replies when this happens)
 	if !o.sendAsync {
+		// wait until the messages have been sent
+		// (nodeStream will produce empty replies when this happens)
 		for range d.Nodes {
 			<-replyChan
 		}
