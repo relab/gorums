a50f20beda54e9aecd1febf7d7f69b62dd1ebe26
Hein Meling <hein.meling@uis.no>
2021-01-16 13:48:17 +0100

    Replaced receiveQueue methods w/newCall in AsyncCall

--- async.go
+++ async.go
@@ -3,7 +3,6 @@ package gorums
 import (
 	"context"
 
-	"github.com/relab/gorums/ordering"
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
@@ -31,23 +30,16 @@ func (f *Async) Done() bool {
 }
 
 func AsyncCall(ctx context.Context, d QuorumCallData) *Async {
-	msgID := d.Manager.nextMsgID()
-	// set up channel to collect replies to this call.
-	replyChan := make(chan *gorumsStreamResult, len(d.Nodes))
-	d.Manager.putChan(msgID, replyChan)
+	expectedReplies := len(d.Nodes)
+	replyChan := make(chan *gorumsStreamResult, expectedReplies)
+	md, callDone := d.Manager.newCall(d.Method, replyChan)
 
-	md := &ordering.Metadata{
-		MessageID: msgID,
-		Method:    d.Method,
-	}
-
-	expected := len(d.Nodes)
 	for _, n := range d.Nodes {
 		msg := d.Message
 		if d.PerNodeArgFn != nil {
 			nodeArg := d.PerNodeArgFn(d.Message, n.id)
 			if nodeArg != nil {
-				expected--
+				expectedReplies--
 				continue
 			}
 		}
@@ -57,7 +49,7 @@ func AsyncCall(ctx context.Context, d QuorumCallData) *Async {
 	fut := &Async{c: make(chan struct{}, 1)}
 
 	go func() {
-		defer d.Manager.deleteChan(msgID)
+		defer callDone()
 		defer close(fut.c)
 
 		var (
@@ -74,8 +66,7 @@ func AsyncCall(ctx context.Context, d QuorumCallData) *Async {
 					errs = append(errs, Error{r.nid, r.err})
 					break
 				}
-				reply := r.reply
-				replies[r.nid] = reply
+				replies[r.nid] = r.reply
 				if resp, quorum = d.QuorumFunction(d.Message, replies); quorum {
 					fut.reply, fut.err = resp, nil
 					return
@@ -84,7 +75,7 @@ func AsyncCall(ctx context.Context, d QuorumCallData) *Async {
 				fut.reply, fut.err = resp, QuorumCallError{"incomplete call", len(replies), errs}
 				return
 			}
-			if len(errs)+len(replies) == expected {
+			if len(errs)+len(replies) == expectedReplies {
 				fut.reply, fut.err = resp, QuorumCallError{"incomplete call", len(replies), errs}
 				return
 			}
