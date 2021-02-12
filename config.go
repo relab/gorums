0c5f405381b6b1c93d09855a724ce57a677ba197
Hein Meling <hein.meling@uis.no>
2021-02-12 22:29:41 +0100

    Added WithNodeIDs option and Equal method
    This adds a WithNodeIDs option for the NewConfiguration function.
    This can be handy when creating a new configuration from a previous one.
    
    The Equal method on the Configuration type is handy for comparing
    two configurations, and also allows using the cmp package for
    comparing stuff.

--- config.go
+++ config.go
@@ -19,11 +19,15 @@ func NewConfiguration(mgr *Manager, opts ...ConfigOption) (nodes Configuration, 
 
 	nodes = make(Configuration, 0)
 	switch {
-	case len(o.addrsList) == 0 && len(o.idMapping) == 0:
-		return nil, ConfigCreationError(fmt.Errorf("no nodes provided; need WithNodeMap or WithNodeList"))
+	case len(o.addrsList) == 0 && len(o.idMapping) == 0 && len(o.nodeIDs) == 0:
+		return nil, ConfigCreationError(fmt.Errorf("no nodes provided; need WithNodeMap or WithNodeList or WithNodeIDs"))
 
 	case len(o.addrsList) > 0 && len(o.idMapping) > 0:
 		return nil, ConfigCreationError(fmt.Errorf("multiple node lists provided; use only one of WithNodeMap or WithNodeList"))
+	case len(o.addrsList) > 0 && len(o.nodeIDs) > 0:
+		return nil, ConfigCreationError(fmt.Errorf("multiple node lists provided; use only one of WithNodeList or WithNodeIDs"))
+	case len(o.idMapping) > 0 && len(o.nodeIDs) > 0:
+		return nil, ConfigCreationError(fmt.Errorf("multiple node lists provided; use only one of WithNodeMap or WithNodeIDs"))
 
 	case len(o.idMapping) > 0:
 		for naddr, id := range o.idMapping {
@@ -57,7 +61,18 @@ func NewConfiguration(mgr *Manager, opts ...ConfigOption) (nodes Configuration, 
 			}
 			nodes = append(nodes, node)
 		}
+
+	case len(o.nodeIDs) > 0:
+		for _, id := range o.nodeIDs {
+			node, found := mgr.Node(id)
+			if !found {
+				// Node IDs must have been registered previously
+				return nil, ConfigCreationError(fmt.Errorf("node ID %d not found", id))
+			}
+			nodes = append(nodes, node)
+		}
 	}
+
 	// Sort nodes to ensure deterministic iteration.
 	OrderedBy(ID).Sort(mgr.nodes)
 	OrderedBy(ID).Sort(nodes)
@@ -81,6 +96,19 @@ func (c Configuration) Nodes() []*Node {
 // Size returns the number of nodes in this configuration.
 func (c Configuration) Size() int {
 	return len(c)
+}
+
+// Equal returns true if configurations b and c have the same set of nodes.
+func (c Configuration) Equal(b Configuration) bool {
+	if len(c) != len(b) {
+		return false
+	}
+	for i := range c {
+		if c[i].ID() != b[i].ID() {
+			return false
+		}
+	}
+	return true
 }
 
 // newCall returns unique metadata for a method call.
--- config_test.go
+++ config_test.go
@@ -3,6 +3,7 @@ package gorums_test
 import (
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/relab/gorums"
 )
 
@@ -63,5 +64,41 @@ func TestNewConfigurationNodeMap(t *testing.T) {
 		if nodeMap[node.Address()] != node.ID() {
 			t.Errorf("mgr.Nodes()[%s] = %d, expected %d", node.Address(), node.ID(), nodeMap[node.Address()])
 		}
+	}
+}
+
+func TestNewConfigurationNodeIDs(t *testing.T) {
+	mgr := gorums.NewManager(gorums.WithNoConnect())
+	c1, err := gorums.NewConfiguration(mgr, gorums.WithNodeList(nodes))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c1.Size() != len(nodes) {
+		t.Errorf("c1.Size() = %d, expected %d", c1.Size(), len(nodes))
+	}
+
+	// Identical configurations c1 == c2
+	nodeIDs := c1.NodeIDs()
+	c2, err := gorums.NewConfiguration(mgr, gorums.WithNodeIDs(nodeIDs))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c2.Size() != len(nodes) {
+		t.Errorf("c2.Size() = %d, expected %d", c2.Size(), len(nodes))
+	}
+	if diff := cmp.Diff(c1, c2); diff != "" {
+		t.Errorf("Expected same configurations, but got (-c1 +c2):\n%s", diff)
+	}
+
+	// Configuration with one less node |c3| == |c1| - 1
+	c3, err := gorums.NewConfiguration(mgr, gorums.WithNodeIDs(nodeIDs[:len(nodeIDs)-1]))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c3.Size() != len(nodes)-1 {
+		t.Errorf("c3.Size() = %d, expected %d", c3.Size(), len(nodes)-1)
+	}
+	if diff := cmp.Diff(c1, c3); diff == "" {
+		t.Errorf("Expected different configurations, but got not difference")
 	}
 }
