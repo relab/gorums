d01de1eb7cf6f743c2f7c4d5d7d8d779abb74a61
Hein Meling <hein.meling@uis.no>
2022-03-04 07:45:45 -0500

    A few more attempts to fix the number of all-to-all connections
    This still contains the old versions that don't work, but I've
    disabled them. Things should be cleaned up now that the new
    version (seems to) work. Have been able to start 200 servers.

--- tests/retry/alltoallconfig_test.go
+++ tests/retry/alltoallconfig_test.go
@@ -13,12 +13,12 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-var (
-	replicaCount = flag.Int("replicaCount", 80, "number of replicas to create all-to-all communication")
-	waitTime     = flag.Int("waitTime", 2, "Seconds to wait before forming the configuration")
-)
+var replicaCount = flag.Int("replicas", 20, "number of replicas to create all-to-all communication")
 
-func TestAllToAllConfigurationStyle1(t *testing.T) {
+// waitTime     = flag.Int("wait", 100, "milliseconds to wait before dialing the configuration replicas")
+// timeout      = flag.Duration("timeout", 100*time.Millisecond, "duration to wait before dialing the configuration replicas")
+
+func disabledTestAllToAllConfigurationStyle1(t *testing.T) {
 	nodeMap := make(map[string]uint32)
 	replicas := make([]*replica, 0)
 	for i := 1; i <= *replicaCount; i++ {
@@ -52,7 +52,7 @@ func TestAllToAllConfigurationStyle1(t *testing.T) {
 	t.Log("Successful TestAllToAllConfigurationStyle1 completion")
 }
 
-func TestAllToAllConfigurationStyle2(t *testing.T) {
+func disabledTestAllToAllConfigurationStyle2(t *testing.T) {
 	replicas, err := createReplicas()
 	if err != nil {
 		t.Fatal(err)
@@ -77,8 +77,43 @@ func TestAllToAllConfigurationStyle2(t *testing.T) {
 	t.Log("Successful TestAllToAllConfigurationStyle2 completion")
 }
 
+func TestAllToAllConfigurationStyle3(t *testing.T) {
+	srvs := make([]*replica, *replicaCount)
+	for i := range srvs {
+		srvs[i] = &replica{}
+	}
+	addrs, closeServers := gorums.TestSetup(t, *replicaCount, func(i int) gorums.ServerIface {
+		srv := gorums.NewServer()
+		RegisterSampleServer(srv, srvs[i])
+		return srv
+	})
+	for i := range srvs {
+		srvs[i].address = addrs[i]
+		srvs[i].id = uint32(i)
+	}
+	nodeMap := make(map[string]uint32)
+	for _, replica := range srvs {
+		nodeMap[replica.address] = replica.id
+	}
+	for _, replica := range srvs {
+		replica.createConfiguration(nodeMap)
+	}
+
+	teardown := func() {
+		for _, replica := range srvs {
+			// replica.stopServer()
+			replica.mgr.Close()
+		}
+		closeServers()
+	}
+	teardown()
+	t.Log("Successful TestAllToAllConfigurationStyle3 completion")
+}
+
 func createReplicas() ([]*replica, error) {
 	replicas := make([]*replica, 0)
+	errChan := make(chan error, *replicaCount)
+	defer close(errChan)
 	for i := 1; i <= *replicaCount; i++ {
 		lis, err := net.Listen("tcp", "127.0.0.1:0")
 		if err != nil {
@@ -88,8 +123,26 @@ func createReplicas() ([]*replica, error) {
 			address: lis.Addr().String(),
 			id:      uint32(i),
 			lis:     lis,
+			server:  gorums.NewServer(),
 		}
+		RegisterSampleServer(replica.server, replica)
 		replicas = append(replicas, &replica)
+	}
+
+	for _, replica := range replicas {
+		replica := replica
+		go func() {
+			if err := replica.serve(); err != nil {
+				errChan <- fmt.Errorf("failed to serve at %q: %w", replica.address, err)
+			}
+		}()
+	}
+
+	select {
+	case err := <-errChan:
+		return nil, err
+	case <-time.After(1000 * time.Millisecond):
+		// slept for a bit to allow replica serve goroutines to fail
 	}
 	return replicas, nil
 }
@@ -105,6 +158,7 @@ type replica struct {
 	lis     net.Listener
 	id      uint32
 	server  *gorums.Server
+	mgr     *Manager
 }
 
 func (r replica) WriteQC(ctx gorums.ServerCtx, request *WriteRequest) (response *WriteResponse, err error) {
@@ -112,30 +166,14 @@ func (r replica) WriteQC(ctx gorums.ServerCtx, request *WriteRequest) (response 
 }
 
 func (r *replica) createConfiguration(nodeMap map[string]uint32) error {
-	srv := gorums.NewServer()
-	r.server = srv
-	RegisterSampleServer(srv, r)
-	errChan := make(chan error)
-	go func() {
-		if err := srv.Serve(r.lis); err != nil {
-			errChan <- fmt.Errorf("failed to serve at %q: %w", r.address, err)
-		}
-	}()
-	time.Sleep(time.Duration(*waitTime) * time.Second)
-	mgr := NewManager(gorums.WithDialTimeout(1*time.Second),
+	r.mgr = NewManager(gorums.WithDialTimeout(100*time.Millisecond),
 		gorums.WithGrpcDialOptions(
 			grpc.WithBlock(), // block until connections are made
 			grpc.WithTransportCredentials(insecure.NewCredentials()), // disable TLS
 		),
 	)
-	qspec := qspec{}
-	_, err := mgr.NewConfiguration(qspec, gorums.WithNodeMap(nodeMap))
-	if err != nil {
-		return err
-	}
-	close(errChan)
-	// since errChan is closed, this should either return an error or nil
-	return <-errChan
+	_, err := r.mgr.NewConfiguration(qspec{}, gorums.WithNodeMap(nodeMap))
+	return err
 }
 
 func (r *replica) startListener() error {
@@ -145,6 +183,10 @@ func (r *replica) startListener() error {
 	}
 	r.lis = lis
 	return nil
+}
+
+func (r *replica) serve() error {
+	return r.server.Serve(r.lis)
 }
 
 func (r *replica) stopServer() {
