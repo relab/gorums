964d6fd2940b03e08bc94a53b868c85308f88840
asbjorn <asbjorn.salhus@gmail.com>
2025-01-07 19:48:40 +0100

    removed gorums.WithDialTimeout

--- opts.go
+++ opts.go
@@ -29,12 +29,6 @@ func newManagerOptions() managerOptions {
 // ManagerOption provides a way to set different options on a new Manager.
 type ManagerOption func(*managerOptions)
 
-// WithDialTimeout returns a ManagerOption which is used to set the dial
-// context timeout to be used when initially connecting to each node in its pool.
-func WithDialTimeout(timeout time.Duration) ManagerOption {
-	return func(o *managerOptions) {}
-}
-
 // WithGrpcDialOptions returns a ManagerOption which sets any gRPC dial options
 // the Manager should use when initially connecting to each node in its pool.
 func WithGrpcDialOptions(opts ...grpc.DialOption) ManagerOption {
