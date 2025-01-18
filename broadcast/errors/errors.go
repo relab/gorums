38a909ad0232b2809b155f99164351581a02b488
Aleksander Vedvik <vedvik.aleksander@gmail.com>
2025-01-18 13:19:25 +0100

    refactor(Broadcast): removed unused errs and added docs

--- broadcast/errors/errors.go
+++ broadcast/errors/errors.go
@@ -1,53 +1,45 @@
-package broadcastErrors
+package errors
 
-type BroadcastIDErr struct{}
+// IDErr should be used when a message with a BroadcastID is sent to a broadcast processor with another BroadcastID. This
+// can happen if a user deliberately changes the BroadcastID of a message.
+type IDErr struct{}
 
-func (err BroadcastIDErr) Error() string {
+func (err IDErr) Error() string {
 	return "wrong broadcastID"
 }
 
+// MissingClientReqErr signifies that a server tries to reply to a client, but has not yet received the original request
+// form the client. This is especially important when the message does not contain routing information, such as in QuorumCalls.
 type MissingClientReqErr struct{}
 
 func (err MissingClientReqErr) Error() string {
 	return "has not received client req yet"
 }
 
+// AlreadyProcessedErr is used when a message is received after the broadcast processor has stopped. This means that the
+// server has sent a reply to the client and thus the incoming message needs not be processed.
 type AlreadyProcessedErr struct{}
 
 func (err AlreadyProcessedErr) Error() string {
 	return "already processed request"
 }
 
-type ReqFinishedErr struct{}
-
-func (err ReqFinishedErr) Error() string {
-	return "request has terminated"
-}
-
+// ClientReqAlreadyReceivedErr should be used when a duplicate client request is received.
 type ClientReqAlreadyReceivedErr struct{}
 
 func (err ClientReqAlreadyReceivedErr) Error() string {
-	return "the client req has already been received. The forward req is thus dropped."
+	return "client request already received (dropped)"
 }
 
-type MissingReqErr struct{}
-
-func (err MissingReqErr) Error() string {
-	return "a request has not been created yet."
-}
-
+// OutOfOrderErr should be used when the preserve ordering configuration option is used and a message is received out of
+// order.
 type OutOfOrderErr struct{}
 
 func (err OutOfOrderErr) Error() string {
 	return "the message is out of order"
 }
 
-type ShardDownErr struct{}
-
-func (err ShardDownErr) Error() string {
-	return "the shard is down"
-}
-
+// InvalidAddrErr should be used when an invalid server/client address is provided.
 type InvalidAddrErr struct {
 	Addr string
 }
