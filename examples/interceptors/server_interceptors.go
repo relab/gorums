39a0ea1fe7de06a3e3871be619e5c1fc2969932c
JosteinLindhom <jossi2010@gmail.com>
2025-06-25 12:20:00 +0200

    refactor: update interceptor signatures to use unified handler type and simplify logic

--- examples/interceptors/server_interceptors.go
+++ examples/interceptors/server_interceptors.go
@@ -10,34 +10,26 @@ import (
 	"google.golang.org/grpc/peer"
 )
 
-func LoggingInterceptor(addr string) func(ctx gorums.ServerCtx, msg *gorums.Message, out chan<- *gorums.Message, next gorums.RequestHandler) {
-	return func(ctx gorums.ServerCtx, msg *gorums.Message, out chan<- *gorums.Message, next gorums.RequestHandler) {
+func LoggingInterceptor(addr string) func(ctx gorums.ServerCtx, msg *gorums.Message, next gorums.Handler) (*gorums.Message, error) {
+	return func(ctx gorums.ServerCtx, msg *gorums.Message, next gorums.Handler) (*gorums.Message, error) {
 		log.Printf("[%s]: LoggingInterceptor(incoming): Method=%s, Message=%s", addr, msg.Metadata.GetMethod(), msg.Message)
 		start := time.Now()
+		resp, err := next(ctx, msg)
 
-		intercepted := make(chan *gorums.Message, 1)
-
-		go func() {
-			defer close(intercepted)
-			next(ctx, msg, intercepted)
-		}()
-
-		for resp := range intercepted {
-			duration := time.Since(start)
-			log.Printf("[%s]: LoggingInterceptor(outgoing): Method=%s, Duration=%s, ResponseErr=%v, Message=%v, Type=%T", addr, msg.Metadata.GetMethod(), duration, resp.Metadata, resp.Message, resp.Message)
-			out <- resp // forward it to the actual output
-		}
+		duration := time.Since(start)
+		log.Printf("[%s]: LoggingInterceptor(outgoing): Method=%s, Duration=%s, ResponseErr=%v, Message=%v, Type=%T", addr, msg.Metadata.GetMethod(), duration, resp.Metadata, resp.Message, resp.Message)
+		return resp, err
 	}
 }
 
-func LoggingSimpleInterceptor(ctx gorums.ServerCtx, msg *gorums.Message, next gorums.ResponseHandler) (*gorums.Message, error) {
+func LoggingSimpleInterceptor(ctx gorums.ServerCtx, msg *gorums.Message, next gorums.Handler) (*gorums.Message, error) {
 	log.Printf("LoggingSimpleInterceptor(incoming): Method=%s, Message=%v)", msg.Metadata.GetMethod(), msg.Message)
 	resp, err := next(ctx, msg)
 	log.Printf("LoggingSimpleInterceptor(outgoing): Method=%s, ResponseErr=%v, Message=%v", msg.Metadata.GetMethod(), err, resp.Message)
 	return resp, err
 }
 
-func DelayedInterceptor(ctx gorums.ServerCtx, msg *gorums.Message, out chan<- *gorums.Message, next gorums.RequestHandler) {
+func DelayedInterceptor(ctx gorums.ServerCtx, msg *gorums.Message, next gorums.Handler) (*gorums.Message, error) {
 	// delay based on sending node address
 	delay := 0 * time.Millisecond
 	peer, ok := peer.FromContext(ctx)
@@ -53,29 +45,24 @@ func DelayedInterceptor(ctx gorums.ServerCtx, msg *gorums.Message, out chan<- *g
 
 	time.Sleep(delay)
 	// Call the next handler in the chain
-	next(ctx, msg, out)
+	resp, err := next(ctx, msg)
 	log.Printf("DelayedInterceptor: Finished processing message after %s", delay)
+	return resp, err
 }
 
 /** NoFooAllowedInterceptor rejects requests for messages with key "foo". */
-func NoFooAllowedInterceptor[T interface{ GetKey() string }](ctx gorums.ServerCtx, msg *gorums.Message, out chan<- *gorums.Message, next gorums.RequestHandler) {
+func NoFooAllowedInterceptor[T interface{ GetKey() string }](ctx gorums.ServerCtx, msg *gorums.Message, next gorums.Handler) (*gorums.Message, error) {
 	if req, ok := msg.Message.(T); ok {
 		log.Printf("NoFooAllowedInterceptor: Received request for key '%s'", req.GetKey())
 		if req.GetKey() == "foo" {
 			log.Printf("NoFooAllowedInterceptor: Rejecting request for key 'foo'")
-			out <- gorums.WrapMessage(msg.Metadata, nil, fmt.Errorf("requests for key 'foo' are not allowed"))
-			// TODO(jostein): We need to release the handler lock if we do not pass the request to the actual handler.
-			// TODO(jostein): However, we need to determine if the lock must be held until we exit the interceptor chain
-			// TODO(jostein): or if it can safely be released in generated code.
-			// TODO(jostein): An alternative is to use a default interceptor that always releases the lock after the interceptor chain has been executed.
-			ctx.Release() // release handler lock to allow other requests to proceed
-			return
+			return nil, fmt.Errorf("requests for key 'foo' are not allowed")
 		}
 	}
-	next(ctx, msg, out)
+	return next(ctx, msg)
 }
 
-func MetadataInterceptor(ctx gorums.ServerCtx, msg *gorums.Message, out chan<- *gorums.Message, next gorums.RequestHandler) {
+func MetadataInterceptor(ctx gorums.ServerCtx, msg *gorums.Message, next gorums.Handler) (*gorums.Message, error) {
 	log.Printf("MetadataInterceptor: Adding custom metadata to message(customKey=customValue)")
 	// Add a custom metadata field
 	entry := ordering.MetadataEntry_builder{
@@ -86,6 +73,7 @@ func MetadataInterceptor(ctx gorums.ServerCtx, msg *gorums.Message, out chan<- *
 		entry,
 	})
 	// Call the next handler in the chain
-	next(ctx, msg, out)
+	resp, err := next(ctx, msg)
 	log.Printf("MetadataInterceptor: Finished processing message with custom metadata")
+	return resp, err
 }
