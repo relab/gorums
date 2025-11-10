051a46c1d4c53f010075dd0146532c943a103742
Hein Meling <hein.meling@uis.no>
2025-11-10 16:13:56 +0100

    feat: add interceptors to the storage repl example

--- examples/storage/server.go
+++ examples/storage/server.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/relab/gorums"
+	"github.com/relab/gorums/examples/interceptors"
 	pb "github.com/relab/gorums/examples/storage/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -26,7 +27,11 @@ func startServer(address string) (*gorums.Server, string) {
 	storage.logger = log.New(os.Stderr, fmt.Sprintf("%s: ", lis.Addr()), log.Ltime|log.Lmicroseconds|log.Lmsgprefix)
 
 	// create Gorums server
-	srv := gorums.NewServer()
+	srv := gorums.NewServer(gorums.WithInterceptors(
+		interceptors.LoggingSimpleInterceptor,
+		interceptors.NoFooAllowedInterceptor[*pb.WriteRequest],
+		interceptors.MetadataInterceptor,
+	))
 	// register server implementation with Gorums server
 	pb.RegisterStorageServer(srv, storage)
 	// handle requests on listener
