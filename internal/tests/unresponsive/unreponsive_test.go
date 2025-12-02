ab4af0b3d928582799a1fd146effb8304f337978
Hein Meling <hein.meling@uis.no>
2025-12-01 19:10:12 -0800

    fix: reduce iteration count for Unresponsive test

--- internal/tests/unresponsive/unreponsive_test.go
+++ internal/tests/unresponsive/unreponsive_test.go
@@ -20,10 +20,10 @@ func (srv testSrv) TestUnresponsive(ctx gorums.ServerCtx, _ *Empty) (resp *Empty
 
 // TestUnresponsiveServer checks that the client is not blocked when the server is not receiving messages
 func TestUnresponsiveServer(t *testing.T) {
+	// defer goleak.VerifyNone(t)
 	addrs, teardown := gorums.TestSetup(t, 1, func(_ int) gorums.ServerIface {
 		gorumsSrv := gorums.NewServer()
-		srv := &testSrv{}
-		RegisterUnresponsiveServer(gorumsSrv, srv)
+		RegisterUnresponsiveServer(gorumsSrv, &testSrv{})
 		return gorumsSrv
 	})
 	defer teardown()
@@ -40,7 +40,7 @@ func TestUnresponsiveServer(t *testing.T) {
 
 	node := cfg[0]
 
-	for range 1000 {
+	for range 100 {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
 		_, err = TestUnresponsive(gorums.WithNodeContext(ctx, node), &Empty{})
 		if err != nil && errors.Is(err, context.Canceled) {
