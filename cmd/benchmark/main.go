07aecd0d419d32806cc64c46a345738351862a5f
Hein Meling <hein.meling@uis.no>
2026-03-25 13:19:49 +0100

    refactor: update NewConfig signature and usage in benchmark app

--- cmd/benchmark/main.go
+++ cmd/benchmark/main.go
@@ -195,12 +195,9 @@ func main() {
 		),
 		gorums.WithSendBufferSize(*sendBuffer),
 	}
-
-	mgr := benchmark.NewManager(dialOpts...)
-	defer mgr.Close()
-
-	cfg, err := benchmark.NewConfiguration(mgr, gorums.WithNodeList(remotes[:options.NumNodes]))
+	cfg, err := benchmark.NewConfig(gorums.WithNodeList(remotes[:options.NumNodes]), dialOpts...)
 	checkf("Failed to create configuration: %v", err)
+	defer cfg.Close()
 
 	results, err := benchmark.RunBenchmarks(benchReg, options, cfg)
 	checkf("Error running benchmarks: %v", err)
